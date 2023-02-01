c78b7081a3ef37011c228a3936b4c9b74ded257b
Jade Phoreman <60015396+Jade-P@users.noreply.github.com>
2023-02-01 14:15:50 -0800

    Add files via upload

---
+++ ECS189G_Winter_2022_Source_Code_Template/code/stage_2_code/Dataset_Loader.py
@@ -0,0 +1,27 @@
+'''
+Concrete IO class for a specific dataset
+'''
+
+# Copyright (c) 2017-Current Jiawei Zhang <jiawei@ifmlab.org>
+# License: TBD
+
+from code.base_class.dataset import dataset
+import pandas as pd
+
+
+class Dataset_Loader(dataset):
+    data = None
+    dataset_source_folder_path = None
+    dataset_source_file_name = None
+    
+    def __init__(self, dName=None, dDescription=None):
+        super().__init__(dName, dDescription)
+    
+    def load(self):
+        print('loading data...')
+        X = []
+        y = []
+
+        train_data = pd.read_csv
+
+        return {'X': X, 'y': y}
